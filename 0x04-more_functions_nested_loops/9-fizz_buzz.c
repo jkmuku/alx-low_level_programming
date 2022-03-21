9e37e0bf775e4fe1561847aa3fdddd36fc486592
jkmuku <mukujohn6@gmail.com>
2022-03-21 00:06:00 +0000

    used a seperate printf for space

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,18 +13,24 @@ int main(void)
 	{
 		if (a % 15 == 0)
 		{
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
+			printf(" ");
 		}
 		else if (a % 3 == 0)
 		{
-			printf("Fizz ");
+			printf("Fizz");
+			printf(" ");
 		}
 		else if (a % 5 == 0)
 		{
 			printf("Buzz ");
+			printf(" ");
 		}
 		else
-			printf("%d ", a);
+		{
+			printf("%d", a);
+			printf(" ");
+		}
 	}
 	printf("\n");
 	return (0);
